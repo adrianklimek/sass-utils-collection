d30b8908747184c285843d652eb8ee6d88613348
Adrian Klimek <adrianklimek@users.noreply.github.com>
2018-08-04 13:24:11 +0200

    Set up test runner

---
+++ test.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const glob = require('glob')
+const True = require('sass-true')
+
+glob
+  .sync(path.resolve(__dirname, '**/*.spec.scss'))
+  .forEach(file => True.runSass({ file }, describe, it))
